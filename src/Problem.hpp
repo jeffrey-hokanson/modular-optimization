dd75c6d3929255aea9473d7eb397176bf2d4b0cb
Jeffrey Hokanson <jeffrey@hokanson.us>
2026-08-16 15:16:29 -0700

    Added gradient support

--- src/Problem.hpp
+++ src/Problem.hpp
@@ -19,7 +19,6 @@ class Problem {
     _objective = obj;
   }
 
-
   std::function<double(Eigen::Ref<Eigen::VectorXd>)> getObjective() 
   {
     return [&](Eigen::Ref<Eigen::VectorXd> state) {
@@ -32,12 +31,32 @@ class Problem {
     return _objective_calls;
   }
 
+  // ---  Gradient  ---
+  void setGradient(std::function<Eigen::VectorXd(Eigen::Ref<Eigen::VectorXd>)>&& grad)
+  {
+    _gradient = grad;
+  }
+
+  std::function<Eigen::VectorXd(Eigen::Ref<Eigen::VectorXd>)> getGradient() 
+  {
+    assert (_gradient && "Must have a gradient");
+    return [&](Eigen::Ref<Eigen::VectorXd> state) {
+      ++_gradient_calls;
+      return (*_gradient)(state);
+    };
+  }
+
+  size_t gradientCalls() const {
+    return _gradient_calls;
+  }
+
  private:
   std::function<double(Eigen::Ref<Eigen::VectorXd>)> _objective;
-  std::optional<std::function<Eigen::VectorXd(Eigen::VectorXd)>> _gradient;
-  std::optional<std::function<Eigen::MatrixXd(Eigen::VectorXd)>> _hessian;
+  std::optional<std::function<Eigen::VectorXd(Eigen::Ref<Eigen::VectorXd>)>> _gradient;
+  std::optional<std::function<Eigen::MatrixXd(Eigen::Ref<Eigen::VectorXd>)>> _hessian;
 
   size_t _objective_calls = 0;
+  size_t _gradient_calls = 0;
 
 };
 
